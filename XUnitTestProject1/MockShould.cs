7747c93cbbd67e7e7d605217d0a9677cea6fad99
Dain Reinhart <dain@wolfgamesllc.com>
2018-04-07 11:21:49 -0400

    Fixed property default value set test Added tests for function with argument count tests

---
+++ XUnitTestProject1/MockShould.cs
@@ -0,0 +1,157 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MockTestSuite
+{
+    /// <summary>
+    /// Contains all the interfaces and classes needed to verify Moq
+    /// Basic Moq tests
+    /// </summary>
+    public class MockShould
+    {
+        /// <summary>
+        /// Interfaces mocked in all tests
+        /// </summary>
+        public interface TestMock
+        {
+            /// <summary>
+            /// Associated class to mock
+            /// </summary>
+            TestClass Class { get; set; }
+
+            /// <summary>
+            /// Property mocked during testing
+            /// </summary>
+            int TestProperty { get; set; }
+
+            /// <summary>
+            /// Function mocked during testing
+            /// </summary>
+            /// <returns>an integer</returns>
+            int TestReturn();
+        }
+
+        /// <summary>
+        /// Class associated with the interface during testing
+        /// </summary>
+        public class TestClass
+        {
+            /// <summary>
+            /// Class associated with the base class for testing hierarchy mocking
+            /// </summary>
+            public virtual TestAssociatedClass AssociatedClass { get; set; }
+
+            /// <summary>
+            /// Used for testing hierarchy mocking of functions
+            /// </summary>
+            /// <returns>false</returns>
+            public virtual bool Submit() { return false; }
+
+            /// <summary>
+            /// Used for testing mocking of calling functions with arguments
+            /// on concrete objects
+            /// </summary>
+            /// <param name="value">The test argument</param>
+            /// <returns></returns>
+            public virtual bool Submit(bool value) { return value; }
+        }
+
+        /// <summary>
+        /// An associated class of a class for hierarchy mocking
+        /// </summary>
+        public class TestAssociatedClass
+        {
+            /// <summary>
+            /// Used for testing hierarchy mocking of properties
+            /// </summary>
+            /// <returns>an integer</returns>
+            public virtual int TestProperty { get; set; }
+        }
+
+        /// <summary>
+        /// Test that moq will set a functions return value
+        /// </summary>
+        [Fact]
+        public void SetFunctionToReturn5()
+        {
+            var mock = new Mock<TestMock>();
+            mock.Setup(testMock => testMock.TestReturn())
+                .Returns(5);
+
+            Assert.Equal(5, mock.Object.TestReturn());
+        }
+
+        /// <summary>
+        /// Test that moq will set a properties return value
+        /// </summary>
+        [Fact]
+        public void SetPropertyToReturn5()
+        {
+            var mock = new Mock<TestMock>();
+            mock.Setup(testMock => testMock.TestProperty).Returns(5);
+
+            Assert.Equal(5, mock.Object.TestProperty);
+        }
+
+        /// <summary>
+        /// Test that moq will set the return value of a base object's property
+        /// </summary>
+        [Fact]
+        public void SetAssociatedClassPropertyToReturn5()
+        {
+            var mock = new Mock<TestMock>();
+            mock.Setup(testMock => testMock.Class.AssociatedClass.TestProperty).Returns(5);
+
+            Assert.Equal(5, mock.Object.Class.AssociatedClass.TestProperty);
+        }
+
+        /// <summary>
+        /// Test that Moq will initialize a properties value
+        /// </summary>
+        [Fact]
+        public void InitializePropertyTo5()
+        {
+            var mock = new Mock<TestMock>();
+            mock.SetupProperty(testMock => testMock.TestProperty, 5);
+
+            TestMock _mock = mock.Object;
+
+            Assert.Equal(5, _mock.TestProperty);
+        }
+
+        /// <summary>
+        /// Test that Moq will count number of method calls
+        /// with any argument values passed
+        /// </summary>
+        [Fact]
+        public void CountMethodCalledWithAnyArguments()
+        {
+            var mock = new Mock<TestClass>();
+            mock.Setup(testMock => testMock.Submit());
+
+            mock.Object.Submit(true);
+            mock.Object.Submit(false);
+
+            mock.Verify(testMock => testMock.Submit(It.IsAny<bool>()), Times.Exactly(2));
+        }
+
+        /// <summary>
+        /// Test that Moq will count number of method calls
+        /// with only specific argument values passed
+        /// </summary>
+        [Fact]
+        public void CountMethodCalledWithSpecificArguments()
+        {
+            var mock = new Mock<TestClass>();
+            mock.Setup(testMock => testMock.Submit());
+
+            mock.Object.Submit(true);
+            mock.Object.Submit(false);
+
+            mock.Verify(testMock => testMock.Submit(false), Times.Exactly(1));
+        }
+    }
+}
---
+++ XUnitTestProject1/XUnitTest.cs
@@ -0,0 +1,206 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+using Moq;
+
+namespace XUnitTestSuite
+{
+    public class Fixture
+    {
+        public void Method()
+        {
+            Assert.True(true);
+        }
+    }
+
+    public class XUnitTestShould : IClassFixture<Fixture>, IDisposable
+    {
+        private readonly ITestOutputHelper output;
+
+        public XUnitTestShould(Fixture fixture, ITestOutputHelper testOutputHelper)
+        {
+            Assert.True(true);
+            fixture.Method();
+            output = testOutputHelper;
+            output.WriteLine("Constructing XUnitTest object");
+        }
+
+        public void Dispose()
+        {
+            Assert.True(true);
+            output.WriteLine("Destroying XUnitTest object");
+        }
+
+        [Fact]
+        public void BeTrue()
+        {
+            Assert.True(true);
+            output.WriteLine("Running ShouldBeTrue test");
+        }
+
+        [Fact]
+        public void Befalse()
+        {
+            Assert.False(false);
+            output.WriteLine("Running ShouldBeFalse test");
+        }
+
+        [Fact]
+        public void NotContainHi()
+        {
+            Assert.DoesNotContain("Hi", "Hello");
+        }
+
+        [Fact]
+        public void NotContaing()
+        {
+            Assert.DoesNotContain("g", "Hello");
+        }
+
+        [Fact]
+        public void NotContainp()
+        {
+            Assert.DoesNotContain("p", "Hello");
+        }
+
+        [Fact]
+        public void ContainEmptyString()
+        {
+            Assert.Contains("", " ");
+        }
+
+        [Fact]
+        public void NotContainSpace()
+        {
+            Assert.DoesNotContain(" ", "A");
+        }
+
+        [Fact]
+        public void NotContainValue()
+        {
+            List<int> list = new List<int>() { 5, 6, 7 };
+            Assert.DoesNotContain<int>(1, list);
+        }
+
+        [Fact]
+        public void NotContainList()
+        {
+            List<List<int>> list = new List<List<int>>() { new List<int>() { 1, 2, 3 }, new List<int> { 4, 5, 6} };
+            List<int> expected = new List<int>() { 5, 6, 7 };
+            Assert.DoesNotContain<List<int>>(expected, list);
+        }
+
+        [Fact]
+        public void Containll()
+        {
+            Assert.Contains("ll", "Hello");
+        }
+
+        [Fact]
+        public void ContainValue()
+        {
+            List<int> list = new List<int>() { 5, 6, 7 };
+            Assert.Contains<int>(5, list);
+        }
+
+        [Fact]
+        public void ContainList()
+        {
+            List<List<int>> list = new List<List<int>>() { new List<int>() { 1, 2, 3 }, new List<int> { 4, 5, 6 } };
+            List<int> expected = new List<int>() { 4, 5, 6 };
+            Assert.Contains<List<int>>(expected, list);
+        }
+
+        [Fact]
+        public void BeEmpty()
+        {
+            Assert.Empty(new List<int>());
+        }
+
+        [Fact]
+        public void NotBeEmpty()
+        {
+            Assert.NotEmpty(new List<int>() { 1 });
+        }
+
+        [Fact]
+        public void EqualString()
+        {
+            Assert.Equal("A", "A");
+        }
+
+        [Fact]
+        public void EqualChar()
+        {
+            Assert.Equal('A', 'A');
+        }
+
+        [Fact]
+        public void EqualInt()
+        {
+            Assert.Equal(1, 1);
+        }
+    
+        [Fact]
+        public void EqualLong()
+        {
+            Assert.Equal(1L, 1L);
+        }
+
+        [Fact]
+        public void EqualDoubleToPrecision3()
+        {
+            Assert.Equal(1.11111, 1.11112, 3);
+        }
+
+        [Fact]
+        public void NotEqualDoubleToPrecision6()
+        {
+            Assert.NotEqual(1.11111, 1.11112, 6);
+        }
+
+        
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+//        [InlineData(6)]
+        public void CheckValueIsOdd(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        private bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+
+        [Fact]
+        public void ThrowNotImplementedException()
+        {
+            Exception ex = Assert.Throws<NotImplementedException>(() => NotImplementedException());
+            output.WriteLine(ex.Message);
+            Assert.Equal("The method or operation is not implemented.", ex.Message);
+        }
+
+        private void NotImplementedException()
+        {
+            throw new NotImplementedException();
+        }
+
+        // https://xunit.github.io/#documentation
+        // Other asserts
+        // InRange
+        // IsNotType and IsNotType
+        // IsType
+        // NotInRange
+        // NotNull
+        // NotSame
+        // Null
+        // Same
+        // Throws and Throws
+
+    }
+}
