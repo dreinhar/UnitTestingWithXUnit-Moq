4ea20ac79ab982aee03ec326ffcf49a84d52e461
Dain Reinhart <dain@wolfgamesllc.com>
2018-10-21 20:46:34 -0400

    Added InlineData Theory example

---
+++ XUnitTestProject1/XUnitTheories.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestSuite
+{
+    public class XUnitTheories
+    {
+        /// <summary>
+        /// Constant and basic type paramaterized test
+        /// </summary>
+        /// <param name="value_1">first constant</param>
+        /// <param name="value_2">second constant</param>
+        /// <param name="sum">expected result</param>
+        [Theory]
+        [InlineData(1,2,3)]
+        [InlineData(11,22,33)]
+        public void InjectConstantsTheory(int value_1, int value_2, int sum)
+        {
+            Assert.Equal(sum, value_1 + value_2);
+        }
+    }
+}
