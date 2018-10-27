97163f768c40819591f92294a185891ee98a7e5e
Dain Reinhart <dain@wolfgamesllc.com>
2018-10-27 09:22:19 -0400

    Now run basic and complex type thories with MemberData

--- XUnitTestProject1/XUnitTheories.cs
+++ XUnitTestProject1/XUnitTheories.cs
@@ -6,7 +6,18 @@ using Xunit;
 
 namespace XUnitTestSuite
 {
-    public class XUnitTheories
+    /// <summary>
+    /// Simple complex test object 
+    /// </summary>
+    public class TestObject
+    {
+        /// <summary>
+        /// Complex object value
+        /// </summary>
+        public int Property_1 { get; set; }
+    };
+
+    public class XUnitInLineDataTheories
     {
         /// <summary>
         /// Constant and basic type paramaterized test
@@ -21,7 +32,10 @@ namespace XUnitTestSuite
         {
             Assert.Equal(sum, value_1 + value_2);
         }
+    }
 
+    public class XUnitClassDataTheories
+    {
         private class ClassDataBasicTypeTestGenerator : IEnumerable<object[]>
         {
             private readonly List<object[]> _data = new List<object[]>
@@ -51,17 +65,11 @@ namespace XUnitTestSuite
             Assert.Equal(sum, value_1 + value_2);
         }
 
-        public class TestObject
-        {
-            public int Property_1 { get; set; }
-            public int Property_2 { get; set; }
-        };
-
         private class ClassDataComplexTypeTestGenerator : IEnumerable<object[]>
         {
             public IEnumerator<object[]> GetEnumerator()
             {
-                var obj = new TestObject { Property_1 = 1, Property_2 = 2 };
+                var obj = new TestObject { Property_1 = 1 };
                 yield return new object[]
                 {
                     obj,
@@ -85,6 +93,79 @@ namespace XUnitTestSuite
         /// <param name="expected">expected object</param>
         [Theory]
         [ClassData(typeof(ClassDataComplexTypeTestGenerator))]
+        public void InjectComplexTypeConstantWithTestGenerator(TestObject result, TestObject expected)
+        {
+            Assert.Equal(expected, result);
+        }
+    }
+
+    public class XUnitMemberDataTheories
+    {
+        private class MemberDataTestGenerator
+        {
+            public static IEnumerable<object[]> GetBasicTypeEnumerator()
+            {
+                yield return new object[] { 5, 1, 6 };
+                yield return new object[] { 7, 2, 9 };
+            }
+
+            public static IEnumerable<object[]> GetComplexTypeEnumerator()
+            {
+                var obj = new TestObject { Property_1 = 1 };
+                yield return new object[]
+                {
+                    obj,
+                    obj,
+                };
+
+                yield return new object[]
+                {
+                    obj,
+                    obj,
+                };
+            }
+        };
+
+
+        public static IEnumerable<object[]> GetBasicTypeEnumeratorFromMethod()
+        {
+            yield return new object[] { 5, 1, 6 };
+            yield return new object[] { 7, 2, 9 };
+        }
+
+        /// <summary>
+        /// Constant and basic type paramaterized test
+        /// </summary>
+        /// <param name="value_1">first constant</param>
+        /// <param name="value_2">second constant</param>
+        /// <param name="sum">expected result</param>
+        [Theory]
+        [MemberData(nameof(GetBasicTypeEnumeratorFromMethod))]
+        public void InjectConstantsTheory(int value_1, int value_2, int sum)
+        {
+            Assert.Equal(sum, value_1 + value_2);
+        }
+
+        /// <summary>
+        /// Constant and basic type paramaterized test using generator class
+        /// </summary>
+        /// <param name="value_1">first constant</param>
+        /// <param name="value_2">second constant</param>
+        /// <param name="sum">expected result</param>
+        [Theory]
+        [MemberData(nameof(MemberDataTestGenerator.GetBasicTypeEnumerator), MemberType = typeof(MemberDataTestGenerator))]
+        public void InjectBasicTypeConstantWithTestGenerator(int value_1, int value_2, int sum)
+        {
+            Assert.Equal(sum, value_1 + value_2);
+        }
+
+        /// <summary>
+        /// Complex type paramaterized test using generator class
+        /// </summary>
+        /// <param name="result">result object</param>
+        /// <param name="expected">expected object</param>
+        [Theory]
+        [MemberData(nameof(MemberDataTestGenerator.GetComplexTypeEnumerator), MemberType = typeof(MemberDataTestGenerator))]
         public void InjectComplexTypeConstantWithTestGenerator(TestObject result, TestObject expected)
         {
             Assert.Equal(expected, result);
